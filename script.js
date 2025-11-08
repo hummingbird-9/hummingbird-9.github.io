e529abfd32a4ebf902104c3bd235c0ad7378bf99
Eashbug <raychuthecreature@gmail.com>
2025-11-07 23:26:07 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const galleryBtn = document.getElementById('galleryBtn');
-    
-    galleryBtn.addEventListener('click', function() {
-        window.location.href = 'gallery.html';
-    });
-});
+
